9c4c7bda020d6ae41a370ae00d079e711d1bf0d6
sairash <gautamsairash@gmail.com>
2024-05-11 07:08:05 +0545

    added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.chainpixels.app',
   appName: 'ChainPixels',
   webDir: 'dist'
 };
