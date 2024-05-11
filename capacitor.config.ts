bec594e7fdd545896a1e474133a26f0d9898a9dc
sairash <gautamsairash@gmail.com>
2024-05-11 07:07:59 +0545

    capacitor js added!

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'ChainPixels',
+  webDir: 'dist'
+};
+
+export default config;
