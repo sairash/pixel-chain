b6ed558265af7121571cfdf6eb557544081a7b12
Bhuwan <bhubanpaudel79@gmail.com>
2024-05-11 17:16:43 +0545

    add logo design first page design event page

--- src/pages/SecondPage.tsx
+++ src/pages/SecondPage.tsx
@@ -36,27 +36,44 @@ const goToEvents =(id:any) =>{
 }
  
   return (
-    <div className="div">
+    <div className="div px-3 py-3">
       <div className="name text-start">
-        <p className="text-2xl text-[#eead0e] transition-all font-serif">
+        <p className="text-2xl  bg-gradient-to-r bg-[#6434cb]  font-bold
+         inline-block text-transparent bg-clip-text transition-all font-serif">
           Your past Events
            </p>
+
       </div>
+      <div className="two">
+      <div className="div mt-5  text-left text-3xl text-[#f3c344] rounded-md
+       hover:bg-[#fff6de]  pl-2  cursor-pointer w-44 pb-2 border-purple-700 font-bold
+       border-dashed border">
+        <p>Memories</p>
+      </div>
+      <div className="div mt-5  text-left text-3xl text-[#f3c344] rounded-md
+         pl-2  cursor-pointer w-28 pb-2 border-purple-700 font-bold
+       border-dashed border">
+        <p>Events</p>
+      </div>
+    
+         </div>
       <div className="event-list flex flex-col gap-10 pt-10 text-xl text-left font-serif">
         {pastEvent &&
           name &&
           name.map((name1:any) => (
             <button key={name1.id}
                onClick={(e) =>goToEvents(name1.id)}
-              className=" text-white bg-[#694978] hover:bg-[#6e4282] py-5 duration-500 rounded-md pl-1 flex gap-5"
+              className=" text-black bg-[#f7f9eb] shadow-md py-5
+               duration-500 rounded-md pl-1 flex gap-5"
             >
               <Avatar 
                 size={40}
                 name={name1.name}
                 variant="beam"
-                colors={["#572d6a", "#990799", "#785687", "#f2cc84", "#ffdede"]}
+                colors={["#a67c00", "	#bf9b30", "#ffbf00", "#ffcf40", "#ffdc73"]}
+                
               />
-              <p  className=" pt-1">Event {name1.name}</p>
+              <p  className=" pt-1 ">Event {name1.name}</p>
             </button>
           ))}
       </div>
@@ -69,8 +86,9 @@ const goToEvents =(id:any) =>{
         <div className="button pt-10 pb-10 fixed lg:-bottom-8 -bottom-5">
           <Button 
           onClick={createEvent}
-          variant={"link"} >
-          <Scan className="h-16 w-14 text-[#694978]" />
+          variant={"link"}  className="relative">
+          <Scan className="h-16 w-14 text-[#694978] "  />
+          <p className="absolute ">scan</p>
           </Button>
         </div>
       </div>
